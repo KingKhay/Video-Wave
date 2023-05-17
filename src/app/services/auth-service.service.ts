f89fb83d1648971f02a32334dffc5bd44728728a
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-17 13:48:36 +0000

    set up the implementation for the login request in the auth service

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  url: string = 'http://localhost:8085/api/auth';
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string){
+    const URL = `${this.url}/login`;
+    const body = {username, password};
+    return this.http.post(URL, body);
+  }
 }
