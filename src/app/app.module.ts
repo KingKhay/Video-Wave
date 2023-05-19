82d34b2a5cda54f28e2b0aca07baa99b7ce8c748
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-19 11:27:07 +0000

    set up first stage of register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CheckboxComponent} from './components/checkbox/checkbox.component';
 import {AuthButtonComponent} from './components/auth-button/auth-button.component';
 import {HttpClientModule} from "@angular/common/http";
+import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
+import { RegisterComponent } from './pages/register/register.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +21,15 @@ import {HttpClientModule} from "@angular/common/http";
     InputComponent,
     CheckboxComponent,
     AuthButtonComponent,
+    RegisterComponent,
+    LogoComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SweetAlert2Module.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  isloading: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(8)],
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    dob: ['', Validators.required]
+  })
+
+  onSubmit() {
+
+  }
 }
