95e27259866916854e232863336e758ee58db88b
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-19 08:58:45 +0000

    created a helper directory for utility purposes

---
+++ src/app/helpers/alert.ts
@@ -0,0 +1,20 @@
+import Swal from "sweetalert2";
+
+export const toastAlert = (icon: any, title: string) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
+  Toast.fire({
+    icon: icon,
+    title: title
+  })
+}
