1c4c8d51a1fd0df40550e2308f0ad5057115cd92
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-04 15:46:36 +0000

    added video component that will display the videos

---
+++ src/app/components/video/video.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.css']
+})
+export class VideoComponent {
+  @Input() videoUrls!: string[];
+}
